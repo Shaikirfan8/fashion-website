847b67f9fe1dec0e9b85a6bb052630fd6bd9af4b
Shaikirfan8 <101869300+Shaikirfan8@users.noreply.github.com>
2026-02-19 01:46:07 +0530

    Create resource.tf

---
+++ .github/workflows/resource.tf
@@ -0,0 +1,32 @@
+resource "google_cloud_run_service" "first_cloud_run" {
+  name     = "irfan-cloudrun"
+  location = "us-central1"
+  project  = "project-586cba26-8a19-4658-b23"
+
+  template {
+    spec {
+      service_account_name = data.google_service_account.terraform.email
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = false
+  }
+}
+
+resource "google_storage_bucket" "irfan_bucket" {
+  name          = "irfan-new-123"
+  location      = "US"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  uniform_bucket_level_access = true   # 🔥 REQUIRED
+
+  versioning {
+    enabled = true
+  }
+}
