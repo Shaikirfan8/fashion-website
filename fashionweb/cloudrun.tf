62c5037ffa771d6ed8d079483b0ab2f6980121d0
Shaikirfan8 <101869300+Shaikirfan8@users.noreply.github.com>
2026-02-18 12:50:11 +0530

    Update cloudrun.tf

--- fashionweb/cloudrun.tf
+++ fashionweb/cloudrun.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_run_service" "first_cloud_run" {
   name     = "irfan-cloudrun"
   location = "us-central1"
-  project  = "project-66a29879-9843-4202-83c"
+  project  = "project-586cba26-8a19-4658-b23"
 
   template {
     spec {
