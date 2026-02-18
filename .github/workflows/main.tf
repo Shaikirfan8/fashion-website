7d07da8b68e1e38ce5f74a57738039ae6d5770db
Shaikirfan8 <101869300+Shaikirfan8@users.noreply.github.com>
2026-02-19 01:35:34 +0530

    Add Terraform configuration for Google provider

---
+++ .github/workflows/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+# Reference the existing Terraform service account
+data "google_service_account" "terraform" {
+  account_id = "terraform"
+}
+
+provider "google" {
+  project = "project-586cba26-8a19-4658-b23"
+  region  = "us-central1"
+}
